3fe640045c9ffa964d2fbe4e1a871ee982b2d2de
MariusBudreika <mariusbudreika09@gmail.com>
2021-03-30 14:44:27 +0300

    js beginning

---
+++ js/02-variables.js
@@ -0,0 +1,44 @@
+/*
+Iniciavimas - vietos isskyrimas kompiuterio atmintyje.
+
+KINTAMUJU INICIAVIMO BUDAI:
+const - konstanta , kintamasis, kurio reiksme negali
+buti keiciama - DEFAULT;
+let - kintamasis, kurio reiksme gali buti keiciama;
+var - kintamasis, kurio reiksme gali buti keiciama, taciau 
+nerekomenduojama naudoti;
+
+KINTAMUJU TIPAI
+tekstas - (string) - rasomas tarp kabuciu;
+
+skaicius - (number) - rasomi skaiciai;
+
+sarasas, masyvas, listas, arejus - (array) - sarasas reiksmiu,
+kuriu tipas gali buti betkoks, taciau rekomenduotina, jog
+sarasa sudarytu vieno tipo reiksmes (lengviau dirbti).
+*/
+
+const name = 'Petras';
+console.log(name);
+
+let balance = 99;
+console.log(balance);
+
+const currency = 'eur';
+console.log(currency);
+
+const skaicius1 = 5;
+console.log(skaicius1);
+
+const skaicius2 = -7.46;
+console.log(skaicius2);
+
+const skaicius3 = -84.3;
+console.log(skaicius3);
+
+const pazymiai = [10, 2, 8, 6, 4];
+console.log(pazymiai);
+
+const studentai = ['Petriukas', 'Onute', 'Maryte', 'Jonukas', 'Jhon']
+console.log(studentai);
+
---
+++ js/03-veiksmai.js
@@ -0,0 +1,38 @@
+const nr1 = 8;
+const nr2 = 5;
+
+const sum = nr1 + nr2;
+console.log(sum);
+
+const minus = nr1 - nr2;
+console.log(minus);
+
+const multi = nr1 * nr2;
+console.log(multi);
+
+const combo = (nr1 + nr2) * (5 / nr2);
+console.log(combo);
+
+const eiliskumas = 1 + 2 - 4 * 5 / 10;
+console.log(eiliskumas);
+
+
+const firstName = 'Vardenis';
+const lastName = 'Pavardenis';
+
+const fullName = firstName + ' ' + lastName + '!';
+console.log(fullName);
+
+const sayHi = 'Labas, ' + firstName + '!';
+console.log(sayHi);
+
+//Parodom, jog kintamuju pavadinimai yra case-sensitive;
+
+const firstname = 'Petras';
+
+console.log(firstName);
+console.log(firstname);
+
+
+
+
