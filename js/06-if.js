92e3834b4e7cfe8b4d4301a250e6209c5d90776b
MariusBudreika <mariusbudreika09@gmail.com>
2021-03-31 15:46:32 +0300

    ternary operator

---
+++ js/06-if.js
@@ -0,0 +1,28 @@
+const city = 'Prienai';
+
+if (city === 'Vilnius' ) {
+    console.log('Sveiki atvyke i Vilniu ;)');
+} else {
+
+        if (city ==='Kaunas') {
+            console.log('Sveiki atvyke i Kauna');
+        } else {
+
+             if (city === 'Siauliai') {
+                 console.log('Sveiki atvyke i Siaulius :D');
+             } else {
+                 console.log('Sveiki atvyke i kazkoki nezinoma miesta :(');
+        }
+    }
+
+}
+
+console.log('-------------');
+
+const arMokausi = false
+
+if (arMokausi === true) {
+    console.log('Taip ir toliau!');
+} else {
+    console.log('Pailseti irgi reikia...');
+}
---
+++ js/07-ternary.js
@@ -0,0 +1,38 @@
+const a = 8;
+const b = 5;
+
+
+// ternary
+console.log( a > b ? 'taip' : 'ne' );
+
+
+// if -else analogas
+if (a > b) {
+    console.log('taip');
+} else {
+    console.log('ne');
+}
+
+const age = 50;
+const agelimit = 18;
+
+const status = age >= agelimit ? 'galiu' : 'dar per jaunas...';
+console.log(status);
+console.log('---------------------');
+
+const kelias = 330;
+const bake = 200;
+
+const arPasieksiuPalanga = kelias > bake ? 'sorry...' : 'yaaay!';
+console.log(arPasieksiuPalanga);
+
+// kaip reikia skaidyti ternary opercija. Gali buti panasus klausymas egze.
+const koksSkaicius2 = 2 < 3 ? 4 < 5 ? 6 : 7 : 8;
+console.log(koksSkaicius2);
+
+const koksSkaicius = 2 < 3 ? 
+                        4 < 5 ? 
+                            6 : 
+                            7 :
+                         8;
+console.log(koksSkaicius);
